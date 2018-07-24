5214d3437700fba5687e483b723a355a56ebb0c0
Raleigh Littles <raleighlittles@gmail.com>
2018-07-23 22:02:58 -0700

    finished chapter 14 (permissions/authorization)

--- customuser/articles/views.py
+++ customuser/articles/views.py
@@ -1,5 +1,6 @@
 # article/views.py
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
@@ -9,30 +10,47 @@ from . import models
 
 
 # Create your views here.
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = models.Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = models.Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Article
     fields = ['title', 'body',]
     template_name = 'article_edit.html'
+    login_url = 'login'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
-
+'''
 class ArticleCreateView(CreateView):
     model = models.Article
     template_name = 'article_new.html'
     # ArticleUpdate only has 2 of these fields because we don't want users to be able to modify the author
     fields = ['title', 'body', 'author',]
+'''
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    # this version supports authorization uses mixins.
+    model = models.Article
+    template_name = 'article_new.html'
+    fields = ['title', 'body']
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance_author = self.request.user
+        return super().form_valid(form)
+
